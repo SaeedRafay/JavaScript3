865a9480ed40671c7af4dda3c78d69b41c435348
Saeed Rafay <saeedahmadrafay@hotmail.com>
2021-01-13 16:47:31 +0100

    just a few improvements in the code

--- hackyourrepo-app/script.js
+++ hackyourrepo-app/script.js
@@ -4,96 +4,9 @@
   Write here your JavaScript for HackYourRepo!
 */
 
-// function #1: to fetch anything from server and return json
-async function fetchData(url) {
-  const response = await fetch(url);
-  return await response.json();
-}
-
-// function #2: to show a repository data upon selection of a repository
-function showRepo() {
-  const repoContainer = document.querySelector("#hyRepo .details");
-  const repoContributors = document.querySelector("#hyRepo .contributors");
-  const repoSelector = document.querySelector("#hyRepo .header select");
-  const repoData = JSON.parse(repoSelector.options[repoSelector.selectedIndex].dataset.repoInfo);
-
-  // remove the data of previously selected repo
-  if (repoContainer.hasChildNodes()) {
-    repoContainer.removeChild(repoContainer.childNodes[0]);
-  }
-
-  const repoDL = document.createElement("dl");
-  repoDL.setAttribute("class", "card");
-
-  for (let [key, value] of Object.entries(repoData)) {
-    if(key !== "html_url") {
-      const repoDT = document.createElement("dt")
-      const repoDD = document.createElement("dd");
-      repoDT.textContent = key;
-
-      if(key === "name") {
-        const repoLink = document.createElement("a");
-        repoLink.textContent = value;
-        repoLink.href = repoData.html_url;
-        repoLink.setAttribute("target", "_blank");
-        repoDD.appendChild(repoLink);
-      } else {
-        repoDD.textContent = value;
-      }
-
-      repoDL.appendChild(repoDT);
-      repoDL.appendChild(repoDD);
-    }
-  }
-
-  repoContainer.appendChild(repoDL);
-
-  const contributorsURL = `https://api.github.com/repos/HackYourFuture/${repoData.name}/contributors`;
-
-  // remove the contributors of previously selected repo
-  const allContributors = document.querySelectorAll("#hyRepo .contributors .contributor");
-  allContributors.forEach(contributor => contributor.parentNode.removeChild(contributor));
-
-  fetchData(contributorsURL)
-    .then(
-      (data) => {
-        data.forEach((contributor) => {
-          const domContributor = document.createElement("div");
-          domContributor.setAttribute("class", "contributor card");
-          repoContributors.appendChild(domContributor);
-
-          const domContributorImg = document.createElement("img");
-          domContributorImg.src = contributor.avatar_url;
-          domContributorImg.width = "50";
-          domContributor.appendChild(domContributorImg);
-
-          const domContributorLink = document.createElement("a");
-          domContributorLink.textContent = contributor.login;
-          domContributorLink.href = contributor.html_url;
-          domContributorLink.target = "_blank";
-          domContributor.appendChild(domContributorLink);
-
-          const domContributions = document.createElement("span");
-          domContributions.setAttribute("class", "commits");
-          domContributions.textContent = contributor.contributions;
-          domContributor.appendChild(domContributions);
-        });
-      },
-      (err) => {
-        displayError(err);
-      }
-    )
-}
-
-// function #3: to show error messages
-function displayError(err) {
-  const errDisplay = document.querySelector("#hyRepo .errors");
-  errDisplay.style.display = "block";
-  errDisplay.textContent = err;
-}
-
-// function #4: main function where all variables are declared and from where all functions are executed
+// main function where all variables are declared and from where all functions are executed
 function main() {
+  // create DOM elements
   document.body.setAttribute("id", "hyRepo");
 
   const domContainer = document.createElement("div");
@@ -108,6 +21,7 @@ function main() {
   domHeader.appendChild(domHeaderTitle);
 
   const domHeaderSelector = document.createElement("select");
+  domHeaderSelector.style.visibility = "hidden";
   domHeader.appendChild(domHeaderSelector);
 
   const domErrorDisplay = document.createElement("div");
@@ -121,6 +35,7 @@ function main() {
 
   const domContributors = document.createElement("section");
   domContributors.setAttribute("class", "contributors");
+  domContributors.style.visibility = "hidden";
   domContainer.appendChild(domContributors);
 
   const domContributorsHead = document.createElement("div");
@@ -133,17 +48,105 @@ function main() {
 
   document.body.appendChild(domContainer);
 
-  // URL for repos
-  const url = "https://api.github.com/orgs/HackYourFuture/repos?per_page=100";
+
+  // function #1: to fetch anything from server and return json
+  async function fetchData(url) {
+    const response = await fetch(url);
+    return await response.json();
+  }
+
+  // function #2: to show a repository data upon selection of a repository
+  function displayError(err) {
+    domErrorDisplay.style.display = "block";
+    domErrorDisplay.textContent = err;
+  }
+
+  // function #3: to show error messages
+  function showRepo() {  
+    // hide any previous errors
+    domErrorDisplay.style.display = "none";
+
+    // remove the data of previously selected repo
+    if (domDetails.hasChildNodes()) {
+      domDetails.removeChild(domDetails.childNodes[0]);
+    }
+
+    // remove the contributors of previously selected repo
+    const allContributors = document.querySelectorAll("#hyRepo .contributors .contributor");
+    allContributors.forEach(contributor => contributor.parentNode.removeChild(contributor));
+  
+    // create a card to display basic repo information
+    const domRepoDL = document.createElement("dl");
+    domRepoDL.setAttribute("class", "card");
+    // get the basic repo data stored previously in the HTML data attribute
+    const repoData = JSON.parse(domHeaderSelector.options[domHeaderSelector.selectedIndex].dataset.repoInfo);
+    // loop through the data from HTML data attribute and create a list
+    for (let [key, value] of Object.entries(repoData)) {
+      // don't write the repo URL
+      if(key !== "html_url") {
+        const domRepoDT = document.createElement("dt")
+        const domRepoDD = document.createElement("dd");
+        domRepoDT.textContent = key;
+        // if looping through repo name then make it a link with repo URL
+        if(key === "name") {
+          const domRepoLink = document.createElement("a");
+          domRepoLink.textContent = value;
+          domRepoLink.href = repoData.html_url;
+          domRepoLink.setAttribute("target", "_blank");
+          domRepoDD.appendChild(domRepoLink);
+        } else {
+          domRepoDD.textContent = value;
+        }
+        domRepoDL.appendChild(domRepoDT);
+        domRepoDL.appendChild(domRepoDD);
+      }
+    }
+    domDetails.appendChild(domRepoDL);
+  
+    // fetch contributors data
+    const contributorsURL = `https://api.github.com/repos/HackYourFuture/${repoData.name}/contributors`;
+    fetchData(contributorsURL)
+      .then(
+        (data) => {
+          data.forEach((contributor) => {
+            // create a card for contributor
+            const domContributor = document.createElement("div");
+            domContributor.setAttribute("class", "contributor card");
+            domContributors.appendChild(domContributor);
+            // add contributor image
+            const domContributorImg = document.createElement("img");
+            domContributorImg.src = contributor.avatar_url;
+            domContributorImg.width = "50";
+            domContributor.appendChild(domContributorImg);
+            // add contibutor name with URL
+            const domContributorLink = document.createElement("a");
+            domContributorLink.textContent = contributor.login;
+            domContributorLink.href = contributor.html_url;
+            domContributorLink.target = "_blank";
+            domContributor.appendChild(domContributorLink);
+            // add the number of contributions
+            const domContributions = document.createElement("span");
+            domContributions.setAttribute("class", "commits");
+            domContributions.textContent = contributor.contributions;
+            domContributor.appendChild(domContributions);
+          });
+        },
+        (err) => {
+          // if there is any error then call the function to display error
+          displayError(err);
+        }
+      )
+  }
 
   // fetch list of repos
+  const url = "https://api.github.com/orgs/HackYourFuture/repos?per_page=100";
   fetchData(url)
     .then(
       (data) => {
         data.forEach((repo, index) => { // create a list of repos in select menu
           const domHeaderSelectorOpt = document.createElement("option");
           domHeaderSelectorOpt.textContent = repo.name;
-          domHeaderSelectorOpt.value = index;
+          domHeaderSelectorOpt.value = index; // storing the index just in case if needed somewhere
           const repoInfo = {
             "name": repo.name,
             "html_url": repo.html_url,
@@ -151,14 +154,18 @@ function main() {
             "forks": repo.forks,
             "updated_at": repo.updated_at,
           }
+          // stored repo related basic data in HTML data attribute to avoid a server request
           domHeaderSelectorOpt.dataset.repoInfo = JSON.stringify(repoInfo);
           domHeaderSelector.appendChild(domHeaderSelectorOpt);
+          domHeaderSelector.style.visibility = "visible";
+          domContributors.style.visibility = "visible";
         });
         
         // show the repo selected by default in the select menu
         showRepo();
       },
       (err) => {
+        // if there is any error then call the function to display error
         displayError(err);
       }
     );
