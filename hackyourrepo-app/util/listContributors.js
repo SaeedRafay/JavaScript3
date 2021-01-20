62d9def9fa252b7089dcfe738c5ae51984336e58
Saeed Rafay <saeedahmadrafay@hotmail.com>
2021-01-20 17:23:20 +0100

    pagination code improved

--- hackyourrepo-app/util/listContributors.js
+++ hackyourrepo-app/util/listContributors.js
@@ -1,22 +1,21 @@
 function listContributors(dom, repoData) {
   const contributorsURL = `https://api.github.com/repos/HackYourFuture/${repoData.name}/contributors`;
   fetchData(contributorsURL)
-  .then(
-    data => {
-      if(data.length < 1) {
-        throw "No Contributors found for this repository.";
+    .then(data => {
+      if (data.length < 1) {
+        throw 'No Contributors found for this repository.';
       }
 
       // creating pages of 5 contributors each
       const dataPages = [];
       for (let i = 0; i < data.length; i += 5) {
-        dataPages.push(data.slice(i, i+5));
+        dataPages.push(data.slice(i, i + 5));
       }
 
       // creating pagination
-      if(dataPages.length > 1) {
+      if (dataPages.length > 1) {
         dom.contributorsPages.style.display = 'block';
-        
+
         dataPages.forEach((dataPage, index) => {
           const pageNum = document.createElement('span');
           pageNum.setAttribute('class', 'pageBtn');
@@ -26,12 +25,12 @@ function listContributors(dom, repoData) {
               e.classList.remove('active');
             });
             this.classList.toggle('active');
-            if(this.previousSibling.classList.contains('prevBtn')) {
+            if (this.previousSibling.classList.contains('prevBtn')) {
               this.previousSibling.classList.add('disabled');
             } else {
               this.parentElement.firstChild.classList.remove('disabled');
             }
-            if(this.nextSibling.classList.contains('nextBtn')) {
+            if (this.nextSibling.classList.contains('nextBtn')) {
               this.nextSibling.classList.add('disabled');
             } else {
               this.parentElement.lastChild.classList.remove('disabled');
@@ -45,15 +44,22 @@ function listContributors(dom, repoData) {
         const nextPage = document.createElement('span');
         prevPage.setAttribute('class', 'pageBtn prevBtn disabled');
         nextPage.setAttribute('class', 'pageBtn nextBtn');
-        prevPage.textContent = "❮";
-        nextPage.textContent = "❯";
+        prevPage.textContent = '❮';
+        nextPage.textContent = '❯';
         prevPage.addEventListener('click', function() {
-          if (this !== dom.contributorsPages.querySelector('.active').previousSibling) {
-            dom.contributorsPages.querySelector('.active').previousSibling.click();
+          if (
+            this !==
+            dom.contributorsPages.querySelector('.active').previousSibling
+          ) {
+            dom.contributorsPages
+              .querySelector('.active')
+              .previousSibling.click();
           }
         });
         nextPage.addEventListener('click', function() {
-          if (this !== dom.contributorsPages.querySelector('.active').nextSibling) {
+          if (
+            this !== dom.contributorsPages.querySelector('.active').nextSibling
+          ) {
             dom.contributorsPages.querySelector('.active').nextSibling.click();
           }
         });
@@ -62,15 +68,12 @@ function listContributors(dom, repoData) {
 
         dom.contributorsPages.childNodes[1].classList.add('active');
       }
-      
+
       // populating contributors list
       pageContributors(dom, dataPages[0]);
-    }
-  )
-  .catch(
-    err => {
+    })
+    .catch(err => {
       // if there is any error then call the function to display error
       displayError(err, dom);
-    }
-  );
+    });
 }
