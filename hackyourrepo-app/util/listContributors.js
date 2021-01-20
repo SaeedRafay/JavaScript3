d1c66a962c220c24ffae5749e610dac6a98fae37
Saeed Rafay <saeedahmadrafay@hotmail.com>
2021-01-20 16:46:41 +0100

    next and previous buttons added to pagination

--- hackyourrepo-app/util/listContributors.js
+++ hackyourrepo-app/util/listContributors.js
@@ -16,6 +16,7 @@ function listContributors(dom, repoData) {
       // creating pagination
       if(dataPages.length > 1) {
         dom.contributorsPages.style.display = 'block';
+        
         dataPages.forEach((dataPage, index) => {
           const pageNum = document.createElement('span');
           pageNum.setAttribute('class', 'pageBtn');
@@ -25,11 +26,41 @@ function listContributors(dom, repoData) {
               e.classList.remove('active');
             });
             this.classList.toggle('active');
+            if(this.previousSibling.classList.contains('prevBtn')) {
+              this.previousSibling.classList.add('disabled');
+            } else {
+              this.parentElement.firstChild.classList.remove('disabled');
+            }
+            if(this.nextSibling.classList.contains('nextBtn')) {
+              this.nextSibling.classList.add('disabled');
+            } else {
+              this.parentElement.lastChild.classList.remove('disabled');
+            }
           });
           pageNum.textContent = index + 1;
           dom.contributorsPages.appendChild(pageNum);
         });
-        dom.contributorsPages.childNodes[0].classList.add('active');
+
+        const prevPage = document.createElement('span');
+        const nextPage = document.createElement('span');
+        prevPage.setAttribute('class', 'pageBtn prevBtn disabled');
+        nextPage.setAttribute('class', 'pageBtn nextBtn');
+        prevPage.textContent = "❮";
+        nextPage.textContent = "❯";
+        prevPage.addEventListener('click', function() {
+          if (this !== dom.contributorsPages.querySelector('.active').previousSibling) {
+            dom.contributorsPages.querySelector('.active').previousSibling.click();
+          }
+        });
+        nextPage.addEventListener('click', function() {
+          if (this !== dom.contributorsPages.querySelector('.active').nextSibling) {
+            dom.contributorsPages.querySelector('.active').nextSibling.click();
+          }
+        });
+        dom.contributorsPages.prepend(prevPage);
+        dom.contributorsPages.append(nextPage);
+
+        dom.contributorsPages.childNodes[1].classList.add('active');
       }
       
       // populating contributors list
